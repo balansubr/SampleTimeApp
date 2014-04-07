029b59b53068f80f8be38dba62c37ac20611f4b6
Balan Subramanian <balan@heroku.com>
2014-04-07 12:13:12 -0700

    first commit

---
+++ app/controllers/clock_controller.rb
@@ -0,0 +1,2 @@
+class ClockController < ApplicationController
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleTimeApp::Application.config.secret_key_base = 'e31ab38f25df267e8e2c636742b626ea0111ac2ccb28a99e54fba914bd36b2b17ff3804add4fcd2c9e02ed78f01fc9d502af939bc722256c46dc0309497c3511'
